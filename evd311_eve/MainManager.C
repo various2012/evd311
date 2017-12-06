4c17c17a21b5afadc2a4eeb0ae511aea3ec0cad5
various2012 <various2012@ukr.net>
2017-12-06 22:24:25 +0200

    Changes to GUI building

--- evd311_eve/MainManager.C
+++ evd311_eve/MainManager.C
@@ -20,6 +20,8 @@
 #include <TGTab.h>
 #include <TGDimension.h>
 #include <TEveViewer.h>
+#include <TGMenu.h>
+
 //class TGlabel;
 
 #ifdef __MAKECINT__
@@ -33,7 +35,7 @@ enum eControlButtons{
 
 };
 
-class MainManager : public TEveManager{
+class MainManager{
 RQ_OBJECT("MainManager")
 private:
 	TEveBox * fLArBox;
@@ -42,11 +44,14 @@ private:
 	TGLAnnotation * fEventCount;
 	//RunSel * fSel;
 	TTree * fCurTree;
-	TGFileBrowser * fFileBrowser;
+	//TGFileBrowser * fFileBrowser;
 	EventManager * fEventManager;
 	TGNumberEntryField * fEntryField;
 	TGLabel * fNumEvtLabel;
 	TEveViewer *fProjectionViewer;
+	TGCanvas * fListTreeCanv;
+	TGListTree * fListTreeFile;
+
 	void MakeEvtGui();//Make gui for event control
 
 	//void HandleButtonsEvt();
@@ -60,48 +65,48 @@ public:
 		fEventCount->SetText(TString::Format("Event %li",fEntryField->GetIntNumber()));
 	};
 	void EntryPressed() {fEventManager->GotoEvent(fEntryField->GetIntNumber());}; //Slot for entry field
-	
+	void HandleFileMenu(Int_t id);
 	void LoadCurrentFile();
 	//void LoadFile();
 
 	ClassDef(MainManager,0);
 };
 
-MainManager::MainManager(UInt_t w, UInt_t h):TEveManager(w,h,kTRUE,"FIV"),
+MainManager::MainManager(UInt_t w, UInt_t h):
 //MainManager::MainManager(UInt_t w, UInt_t h):(&(TEveManager::Create())),
 fLArBox(0),
 fCRPBox(0),
 //fSel(0),
 fCurTree(0),
-fFileBrowser(0),
 fAxis(0),
 fEventCount(0),
 fEventManager(0),
 fEntryField(0),
 fNumEvtLabel(0)
 {
-	//TEveManager::Create();
+	TEveManager::Create();
 	//gStyle->SetMarkerStyle(7);
 	fLArBox = EvdUtils::FidBox("Fiducial Volume");
 	fCRPBox = EvdUtils::CRPPlane("CRP plane");
 	fAxis = EvdUtils::MakeAxis("Axis",15,7,7);
-	fEventCount=EvdUtils::EventCount(GetDefaultGLViewer());
+	fEventCount=EvdUtils::EventCount(gEve->GetDefaultGLViewer());
 	fCurTree=new TTree();
 
 	fEventManager = new EventManager("Event_is_not_loaded");
-	AddEvent(fEventManager);
-	SetCurrentEvent(fEventManager);
-	AddGlobalElement(fLArBox);
-	AddGlobalElement(fCRPBox);
-	AddGlobalElement(fAxis);
+	gEve->AddEvent(fEventManager);
+	gEve->SetCurrentEvent(fEventManager);
+	gEve->AddGlobalElement(fLArBox);
+	gEve->AddGlobalElement(fCRPBox);
+	gEve->AddGlobalElement(fAxis);
 	//AddGlobalElement(fEveCount);
-	GetDefaultGLViewer()->CurrentCamera().SetCenterVec(150,0,0);
+	gEve->GetDefaultGLViewer()->CurrentCamera().SetCenterVec(150,0,0);
 	
 	MakeEvtGui();
 
-	FullRedraw3D();
+	gEve->FullRedraw3D();
+	gEve->GetBrowser()->GetMenuBar()->GetPopup("Browser")->Connect("Activated(Int_t)","MainManager", this, "HandleFileMenu(Int_t)");
 	// fFileBrowser=fBrowser->fFileBrowser;
-	// fFileBrowser->fListTree->Connect("DoubleClicked(TGListTreeItem * , Int_t )","MainManager",this,"ReloadTree(TGListTreeItem * , Int_t )");
+	fListTreeFile->Connect("DoubleClicked(TGListTreeItem * , Int_t )","MainManager",this,"ReloadTree(TGListTreeItem * , Int_t )");
 	//GetMainWindow()->MapWindow();
 
 	
@@ -113,7 +118,9 @@ MainManager::~MainManager(){
 }
 
 void MainManager::ReloadTree(TGListTreeItem * item, Int_t btn){
-	if(!gFile) return;	
+	TFile *tmpFile = (TFile *)item->GetUserData();
+	if(!tmpFile) return;
+	gFile = tmpFile;	
 	TRegexp re("^.+\\.root$");
 	Int_t len;
 	if(re.Index(gFile->GetName(),&len)<0){
@@ -121,7 +128,7 @@ void MainManager::ReloadTree(TGListTreeItem * item, Int_t btn){
 		 return;
 	}
 	//printf ("Len=%i\n",len);
-	if(fCurTree->GetCurrentFile()==gFile) return;
+	//if(fCurTree->GetCurrentFile()==gFile) return;
 	TTree *fTmpTree=(TTree*) gFile->Get("analysistree/anatree");
 	if(fTmpTree==0) return;
 	fCurTree=fTmpTree;
@@ -135,30 +142,34 @@ void MainManager::ReloadTree(TGListTreeItem * item, Int_t btn){
 	fNumEvtLabel->SetText(TString::Format("/%i",nEntries-1));
 	fNumEvtLabel->Resize(fNumEvtLabel->GetDefaultSize());
 	EntryPressed();
-	FullRedraw3D();
+	gEve->FullRedraw3D();
 	//printf("Tree loaded from file %s\n",gFile->GetName());
 }
 
 void MainManager::MakeEvtGui(){
-	//fBrowser->StartEmbedding(TRootBrowser::kLeft);
-	TGTab * ctab = fBrowser->GetTab(TRootBrowser::kLeft);
-	TGCompositeFrame * cont=ctab->AddTab("Event Control");
-	cont->SetLayoutManager(new TGVerticalLayout(cont));
+	gEve->GetBrowser()->StartEmbedding(TRootBrowser::kLeft);
+	//TGTab * ctab = fBrowser->GetTab(TRootBrowser::kLeft);
+	//TGCompositeFrame * cont=ctab->AddTab("Event Control");
+	//cont->SetLayoutManager(new TGVerticalLayout(cont));
 	TGDimension btnsize(20,20);
 	//cont->Layout();
-	//TGMainFrame * evtMain= new TGMainFrame(gClient->GetRoot(),400,400);
-	//evtMain->SetWindowName("Evt Gui");
-	//evtMain->SetCleanup(kDeepCleanup);
+	TGMainFrame * evtMain= new TGMainFrame(gClient->GetRoot(),400,400,kVerticalFrame);
+	evtMain->SetWindowName("Evt Gui");
+	evtMain->SetCleanup(kDeepCleanup);
 	
 	//TGButtonGroup * btnGroup = new TGButtonGroup(evtMain,"Controls");
-	
-	TGHorizontalFrame * fFrame = new TGHorizontalFrame(cont,400,400);
+	fListTreeCanv = new TGCanvas(evtMain,400,400);
+
+	fListTreeFile = new TGListTree(fListTreeCanv,kHorizontalFrame);
+	//fListTreeCanv->AddFrame(fListTree);
+	evtMain->AddFrame(fListTreeCanv,new TGLayoutHints(kLHintsExpandX|kLHintsNormal,2,2,2,2));
+	TGHorizontalFrame * fFrame = new TGHorizontalFrame(evtMain,400,400);
 	fFrame->SetName("fFrame");
-    TGTextButton * bLoadEvent = new TGTextButton(fFrame, "Load");
-	bLoadEvent->SetWidth(35);
-	bLoadEvent->SetHeight(20);
-	bLoadEvent->Connect("Clicked()","MainManager",this,"LoadCurrentFile()");
-	fFrame->AddFrame(bLoadEvent,new TGLayoutHints(kLHintsNormal));
+//    TGTextButton * bLoadEvent = new TGTextButton(fFrame, "Load");
+//	bLoadEvent->SetWidth(35);
+//	bLoadEvent->SetHeight(20);
+//	bLoadEvent->Connect("Clicked()","MainManager",this,"LoadCurrentFile()");
+//	fFrame->AddFrame(bLoadEvent,new TGLayoutHints(kLHintsNormal|kLHintsCenterY));
 	TGPictureButton * bPrev = new TGPictureButton(fFrame, gClient->GetPicture("arrow_left.xpm"),kPrevEvent);
 	bPrev->Connect("Clicked()", "EventManager", fEventManager,"PrevEvent()");
 	bPrev->Connect("Clicked()", "MainManager", this,"SetEntryValue()");
@@ -189,21 +200,23 @@ void MainManager::MakeEvtGui(){
 	bNext->Connect("Clicked()", "EventManager", fEventManager,"NextEvent()");
 	bNext->Connect("Clicked()", "MainManager", this,"SetEntryValue()");
 //	
-	cont->AddFrame(fFrame, new TGLayoutHints(kLHintsNormal|kLHintsExpandX,2,2,2,2));
+	evtMain->AddFrame(fFrame, new TGLayoutHints(kLHintsNormal|kLHintsExpandX,2,2,2,2));
 	//cont->MapSubwindows();
 	//cont->Resize();
 	//cont->Layout();
 	// ----------------------------------------------------
-	fProjectionViewer = SpawnNewViewer("projection", "pepe-");
+
 	//gPad->SetViewer3D(fProjectionViewer->GetGLViewer());
-	ctab->MapSubwindows();
-	ctab->Layout();
-//	evtMain->MapSubwindows();
-//	evtMain->Resize();
-//	evtMain->MapWindow();
+	//ctab->MapSubwindows();
+	//ctab->Layout();
+	evtMain->MapSubwindows();
+	evtMain->Layout();
+	evtMain->Resize();
+	evtMain->MapWindow();
 	//gEve->GetMainWindow()->MapSubwindows();
-	//Browser->StopEmbedding();
-	//fBrowser->SetTabTitle("Event Control",0);
+	gEve->GetBrowser()->StopEmbedding();
+	gEve->GetBrowser()->SetTabTitle("Event Control",0);
+	fProjectionViewer = gEve->SpawnNewViewer("Projections", "pepe-");
 	
 	
 }
@@ -222,9 +235,23 @@ void MainManager::LoadCurrentFile(){
 	fNumEvtLabel->SetText(TString::Format("/%i",nEntries-1));
 	fNumEvtLabel->Resize(fNumEvtLabel->GetDefaultSize());
 	EntryPressed();
-	FullRedraw3D();
+	gEve->FullRedraw3D();
 }
 //void MainManager::SetEntryValue(){
 //	fEntryField->SetIntNumber(fEventManager->GetCurrentEvent());
 //}
+
+void MainManager::HandleFileMenu(Int_t id){
+	Int_t openid = gEve->GetBrowser()->GetMenuBar()->GetPopup("Browser")->GetEntry("Open...")->GetEntryId();
+	printf("Openid: %i\n",openid);
+	if(id==openid){
+		TSeqCollection * fileList = gROOT->GetListOfFiles();
+		TIter * it = new TIter(fileList);
+		TFile * tmpFile;
+		while(tmpFile=(TFile*) it->Next()){
+			fListTreeFile->AddItem(0,tmpFile->GetName(),(void *) tmpFile,gClient->GetPicture("rootdb_t.xpm"),gClient->GetPicture("rootdb_t.xpm"));
+		}
+		
+	}
+}
 
